246175809fb8c07af9e23d1c6154c6e745f74166
Ribusta <56714769+Ribusta@users.noreply.github.com>
2023-06-13 10:55:47 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartService } from './cart.service';
+
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
@@ -18,7 +18,7 @@ import { CartComponent } from './cart/cart.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartService },
+
       { path: 'cart', component: CartComponent },
     ]),
   ],
@@ -28,6 +28,7 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [AppComponent],
 })
